36a0436926b1b6ae6bb84eae3c895eab7837321d
John Rotter <67421720+jrotter2@users.noreply.github.com>
2022-06-16 14:50:26 -0500

    Update copyLutsToL1TMuonEndcap.py

--- copyLutsToL1TMuonEndcap.py
+++ copyLutsToL1TMuonEndcap.py
@@ -7,31 +7,30 @@ import getpass
 import subprocess
 import xml.etree.ElementTree as ET
 
-eospath = "/store/user/dildick/"
+eospath = "/store/user/jrotter/"
 allowedModes = [15, 14, 13, 12, 11, 10, 9, 7, 6, 5, 3]
-label = "Prep2018DataRate_eta1.25to2.4_isRun3"
-label = "v0p"
-xmlFileName = "f_logPtTarg_invPtWgt_BDTG_AWB_Sq.weights.xml"
-tags = ['isRun3_bitComp', 'isRun3_useQS_bitComp', 'isRun3_useQS_useES_bitComp']
-tags = ['isRun3_bitComp']
+label = ""
+xmlFileName = "f_logPtTarg_invlog2PtWgt_BDTG_AWB_Sq.weights.xml" #"f_logPtTarg_invPtWgt_BDTG_AWB_Sq.weights.xml" #"f_invPtTarg_invPtWgt_BDTG_AWB_Sq.weights.xml"
+tags = ['Run2BitComp_NewSample_logPtTarg_invlog2PtWgt_1000MAXTRG_NoRPCDeWgt_e']
 #dirs = ['v1p0', 'v1p1', 'v1p2']
-dirs = ['v0p0', 'v0p1', 'v0p2']
+dirs = ['v3p0']
+targetVar = "logPt" #Options are: logPt, invPt, Pt
 
 cmssw_directory = subprocess.Popen("echo $CMSSW_BASE", shell=True, stdout=subprocess.PIPE).stdout.read().strip('\n')
-target_directory = cmssw_directory + "/src/L1Trigger/L1TMuonEndCap/data/pt_xmls/"
+target_directory = cmssw_directory + "/src/L1Trigger-L1TMuonEndCap/pt_xmls" #"/src/L1Trigger/L1TMuonEndCap/data/pt_xmls"
 
 for mode in allowedModes:
     print(mode)
 
     for itag in range(0,len(tags)):
-        eoscommand = ('''eos root://cmseos.fnal.gov ls /store/user/dildick | grep "EMTF_BDT_Train_Mode{}" | grep "{}" | grep "{}"'''.format(mode, label, tags[itag]))
+        eoscommand = ('''eos root://cmseos.fnal.gov ls /store/user/jrotter | grep "EMTF_BDT_Train_Mode{}" | grep "{}" | grep "{}"'''.format(mode, label, tags[itag]))
         trainings = subprocess.Popen(eoscommand, shell=True, stdout=subprocess.PIPE).stdout.read().strip('\n')
         splittrainings = trainings.split('\n')
         #print(trainings.split('\n'))
 
         for training in splittrainings:
             #print("eos root://cmseos.fnal.gov ls /store/user/dildick/" + training)
-            exec_string = ("xrdcp /eos/uscms/store/user/dildick/" + training + "/" + xmlFileName + " " +
+            exec_string = ("xrdcp /eos/uscms/store/user/jrotter/" + training + "/" + xmlFileName + " " +
                            "{}/{}/".format(target_directory, dirs[itag]) +
                            xmlFileName.replace('.weights.xml', '_mode{}.weights.xml'.format(mode)))
             print(exec_string)
@@ -40,7 +39,7 @@ for mode in allowedModes:
             #continue
 
             ## split up the XML in 400 separate XML files
-            tree = ET.parse('{}/{}/f_logPtTarg_invPtWgt_BDTG_AWB_Sq_mode15.weights.xml'.format(target_directory, dirs[itag]))
+            tree = ET.parse('{}/{}/f_{}Targ_invlog2PtWgt_BDTG_AWB_Sq_mode{}.weights.xml'.format(target_directory, dirs[itag], targetVar,  mode)) #NEED TO CHANGE NAME OF Wgt
             root = tree.getroot()
             weights= root.find('Weights')
 
